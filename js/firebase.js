a1eb2ac371df79c001b4050cd97e0c74d014c729
Snow275 <kasse.coura27@gmail.com>
2025-08-01 16:17:44 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,25 +1,39 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
+
+// 1. Core SDK
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 2. Firestore SDK
+import {
+  getFirestore,
+  collection,
+  doc,
+  onSnapshot,
+  addDoc,
+  updateDoc,
+  deleteDoc
+} from "firebase/firestore";
+
+// 3. Ta configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCZpE-EQ-cS-06gdkOljOelMExOq1t5aos",
   authDomain: "stage-planner-ca.firebaseapp.com",
   projectId: "stage-planner-ca",
-  storageBucket: "stage-planner-ca.firebasestorage.app",
+  storageBucket: "stage-planner-ca.appspot.com",
   messagingSenderId: "347115346113",
   appId: "1:347115346113:web:8e835602f38c374e711c40",
   measurementId: "G-5ZMNGF1YFK"
 };
 
-// Initialize Firebase
+// 4. Initialisation de l’app Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// 5. Instance Firestore
+const db = getFirestore(app);
+
+// 6. Export des utilitaires Firestore pour ton code métier
 export {
   db,
   collection,
