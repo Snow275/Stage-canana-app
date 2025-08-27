725cd1007620d32de3355166fb6ec4077097c210
Snow275 <kasse.coura27@gmail.com>
2025-08-27 14:03:39 +0200

    Update dishes.js

--- js/dishes.js
+++ js/dishes.js
@@ -1,5 +1,9 @@
 // js/dishes.js
-
+// Backendless config
+const BL_APP_ID   = '948A3DAD-06F1-4F45-BECA-A039688312DD';
+const BL_REST_KEY = '8C69AAC6-204C-48CE-A60B-137706E8E183';
+const BL_API_URL  = 'https://api.backendless.com';
+const BL_TABLE    = 'Dishes';
 
 export function initDishes() {
 
@@ -102,8 +106,30 @@ li.querySelector('button').onclick = () => {
 
   li.remove();
 
+  if (navigator.onLine) {
+  blListDishes().then(remote => {
+    remote.forEach(r => {
+      if (!dishes.find(x => x.objectId === r.objectId)) {
+        const item = { id: r.localId || Date.now(), text: r.text, objectId: r.objectId };
+        dishes.push(item);
+        renderDish(item);
+      }
+    });
+    saveAll();
+  }).catch(err => console.warn("BL list KO", err));
+  }
+
 };
 
+// Après renderDish(d);
+if (navigator.onLine) {
+  blCreateDish(d).then(obj => {
+    // stocke l'objectId renvoyé par Backendless
+    d.objectId = obj.objectId;
+    saveAll();
+  }).catch(err => console.warn("BL create KO", err));
+}
+  
 list.appendChild(li);
 
 
@@ -120,6 +146,9 @@ localStorage.setItem('dishes', JSON.stringify(dishes));
 
 }
 
+  if (d.objectId && navigator.onLine) {
+  blDeleteDish(d.objectId).catch(err => console.warn("BL delete KO", err));
+  }
 
 function downloadCSV(filename, text) {
 
@@ -157,3 +186,25 @@ return div.innerHTML;
 
 }
 
+async function blCreateDish(d) {
+  const url = `${BL_API_URL}/${BL_APP_ID}/${BL_REST_KEY}/data/${BL_TABLE}`;
+  const r = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type':'application/json' },
+    body: JSON.stringify({ text: d.text, localId: d.id })
+  });
+  if (!r.ok) throw new Error('Erreur création Backendless');
+  return r.json();
+}
+
+async function blDeleteDish(objectId) {
+  const url = `${BL_API_URL}/${BL_APP_ID}/${BL_REST_KEY}/data/${BL_TABLE}/${objectId}`;
+  await fetch(url, { method: 'DELETE' });
+}
+
+async function blListDishes() {
+  const url = `${BL_API_URL}/${BL_APP_ID}/${BL_REST_KEY}/data/${BL_TABLE}?sortBy=created%20desc`;
+  const r = await fetch(url);
+  if (!r.ok) throw new Error('Erreur lecture Backendless');
+  return r.json();
+}
